52acc9462ae83ae2bdb4b728487c6be2ec6ee3ba
andre <andre@metasolutions.se>
2025-12-12 12:18:37 +0100

    update test to work with new structure

--- test/utils.js
+++ test/utils.js
@@ -28,15 +28,25 @@ const executeSPARQLTest = async (testSuite, testPath) => {
 const executeReportTest = async (testSuite, testPath) => {
   const reportSuite = JSON.parse(fs.readFileSync(`${testSuite}/report.json`, { encoding: 'utf8' }));
   const reportPath = JSON.parse(fs.readFileSync(`${testPath}/report.json`, { encoding: 'utf8' }));
-  expect(reportSuite.added).toBe(reportPath.added);
-  expect(reportSuite.removed).toBe(reportPath.removed);
-  expect(reportSuite.updated).toBe(reportPath.updated);
-  expect(reportSuite.addedURIs.length).toBe(reportPath.addedURIs.length);
-  expect(reportSuite.removedURIs.length).toBe(reportPath.removedURIs.length);
-  expect(reportSuite.updatedURIs.length).toBe(reportPath.updatedURIs.length);
-  expect(reportSuite.addedURIs).toEqual(expect.arrayContaining(reportPath.addedURIs));
-  expect(reportSuite.removedURIs).toEqual(expect.arrayContaining(reportPath.removedURIs));
-  expect(reportSuite.updatedURIs).toEqual(expect.arrayContaining(reportPath.updatedURIs));
+
+  // Get all entity types from both reports
+  const entityTypes = new Set([...Object.keys(reportSuite), ...Object.keys(reportPath)]);
+  const defaults = { added: 0, removed: 0, updated: 0, unchanged: 0, addedURIs: [], removedURIs: [], updatedURIs: [] };
+
+  for (const entityType of entityTypes) {
+    const suiteCounts = reportSuite[entityType] || { ...defaults };
+    const pathCounts = reportPath[entityType] || { ...defaults };
+
+    expect(suiteCounts.added).toBe(pathCounts.added);
+    expect(suiteCounts.removed).toBe(pathCounts.removed);
+    expect(suiteCounts.updated).toBe(pathCounts.updated);
+    expect(suiteCounts.addedURIs.length).toBe(pathCounts.addedURIs.length);
+    expect(suiteCounts.removedURIs.length).toBe(pathCounts.removedURIs.length);
+    expect(suiteCounts.updatedURIs.length).toBe(pathCounts.updatedURIs.length);
+    expect(suiteCounts.addedURIs).toEqual(expect.arrayContaining(pathCounts.addedURIs));
+    expect(suiteCounts.removedURIs).toEqual(expect.arrayContaining(pathCounts.removedURIs));
+    expect(suiteCounts.updatedURIs).toEqual(expect.arrayContaining(pathCounts.updatedURIs));
+  }
 };
 
 export const runTests = async (testSuite, testPath) => {
